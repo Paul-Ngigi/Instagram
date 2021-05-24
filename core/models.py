15352e1443f7499c87c4eeb068c993bc1e96e6c4
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-24 14:23:04 +0300

    add follow model

--- core/models.py
+++ core/models.py
@@ -12,12 +12,12 @@ class Post(models.Model):
     """
     image = CloudinaryField('post')
     caption = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, editable=False)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.image
+        return str(self.pk)
 
 
 class Comment(models.Model):
@@ -26,9 +26,12 @@ class Comment(models.Model):
     """
     text = models.CharField(max_length=240)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_on = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
+    comment_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.text)
 
 
 class Like(models.Model):
@@ -36,8 +39,23 @@ class Like(models.Model):
     Class that holds like models
     """
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
     is_like = models.BooleanField(default=True)
-    created_on = models.DateTimeField(auto_now_add=True)
+    liked_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return str(self.is_like)
+
+
+class Follow(models.Model):
+    """
+    Class that holds follow models
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follow_follower', editable=False)
+    followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follow_followed')
+    followed_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user } --> {self.followed}"
