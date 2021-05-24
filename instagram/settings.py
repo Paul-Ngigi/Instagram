2466b5407093d8db721f32a25644b01841d980c5
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-24 10:39:15 +0300

    update the user model

--- instagram/settings.py
+++ instagram/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'authentication',
     'common',
     'core',
+    'user',
     'cloudinary',
-    'bootstrap5'
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -81,8 +82,10 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
 
@@ -135,3 +138,6 @@ cloudinary.config(
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'user.User'
