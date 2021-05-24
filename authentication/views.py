18e6489e9338800bc2bbcae500cf8be26739aa18
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-24 11:31:41 +0300

    add user sign up

--- authentication/views.py
+++ authentication/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.views.generic import View
+from .forms import UserForm, CustomUserChangeForm
 
 # Create your views here.
 def home(request):
-    pass
+    return HttpResponse('Home')
 
 
 class SignUpView(View):
+    template_name = 'signup.html'
+    user_form = UserForm
+    
     def get(self, request, *args, **kwargs):
-        title = 'Register'
-        return render(request, 'signup.html', {'title': title})
+        return render(request, self.template_name)
     
     def post(self, request, *args, **kwargs):
-        pass
+        form = self.user_form(request.POST)
+        breakpoint()
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+        return render(request, self.template_name, {'form': form})
