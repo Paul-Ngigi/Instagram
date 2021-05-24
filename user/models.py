99343fc9764c8e42f5d06e950fcc8dc3240bdb2f
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-24 10:16:25 +0300

    add user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.base_user import BaseUserManager
 
 # Create your models here.
+class User(AbstractUser):
+    picture = CloudinaryField('profile_pictures')
+    
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+    
+    objects = BaseUserManager()
