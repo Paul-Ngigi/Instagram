2eab0bbebd7d1d4433b806e44b3a6dad8b1b7b64
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-24 10:36:52 +0300

    add managers

--- user/models.py
+++ user/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from cloudinary.models import CloudinaryField
-from django.contrib.auth.base_user import BaseUserManager
+from .managers import CustomUserManager
+
 
 # Create your models here.
 class User(AbstractUser):
-    picture = CloudinaryField('profile_pictures')
+    picture = CloudinaryField('profile_pictures', null=False, blank=False)
+    full_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
     
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
     
-    objects = BaseUserManager()
+    objects = CustomUserManager()
