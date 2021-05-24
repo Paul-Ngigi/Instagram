9d497eedc42351abce7f371801a0a0ba5cf90d59
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-24 11:18:00 +0300

    add sign up template and styling

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
 
 # Create your views here.
+def home(request):
+    pass
+
+
+class SignUpView(View):
+    def get(self, request, *args, **kwargs):
+        title = 'Register'
+        return render(request, 'signup.html', {'title': title})
+    
+    def post(self, request, *args, **kwargs):
+        pass
