a0b56fb72b524aee72bb29b96ce93a64021b840e
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-25 13:40:33 +0300

    add a search view and url

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home_view'),
     path('addpost/', views.add_post, name='addpost_view'),
+    path('search/', views.search_user, name='search_results')
 ]
--- core/views.py
+++ core/views.py
@@ -11,19 +11,33 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def home(request):
     posts = Post.objects.all()
-    
+
     return render(request, 'posts/index.html', {'posts': posts})
+
 
 @login_required
 def add_post(request):
     form = PostCreateForm(request.POST)
-    
+
     if form.is_valid():
         form.save()
         redirect('home_view')
-    
+
     else:
         form = PostCreateForm
-        
-        return render(request, 'posts/add-post.html', {'form':form})
-    
+
+        return render(request, 'posts/add-post.html', {'form': form})
+
+
+@login_required
+def search_user(request):
+    if 'profile' in request.GET and request.GET['profile']:
+        search_term = request.GET.get('profile')
+        searched_user = Post.get_user(search_term)
+        message = f'{searched_user}'
+
+        return render(request, 'all-posts/search.html', {'message': message})
+
+    else:
+        message = "We can't seem to find what you are looking for"
+        return render(request, 'all-posts/search.html', {'message': message})
